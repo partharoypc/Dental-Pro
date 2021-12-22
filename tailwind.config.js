c62720038379cdb9b170ac20a80278d7566b2b65
Partha Roy <partharoypc@gmail.com>
2021-12-22 22:32:12 +0600

    Dental Pro
    All project app

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+
+                "demo-gray": "#adadad",
+                "demo-gray2": "#666",
+                "btn-bg": "#DCA378",
+            },
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
